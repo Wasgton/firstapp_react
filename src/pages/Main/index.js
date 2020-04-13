291634161b7ec3122cd828f85ec6e7600b45d3fe
wasgton <wasgton.it@gmail.com>
2020-04-13 15:16:39 -0300

    Estilização

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import { Title } from './style';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './style';
 
 export default function Main() {
-    return <Title>Main</Title>;
+    return (
+        <Container>
+            <h1>
+                <FaGithubAlt />
+                Repositórios
+            </h1>
+            <Form onSubmit={() => {}}>
+                <input type="text" placeholder="Adicionar Repositório" />
+
+                <SubmitButton>
+                    <FaPlus />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 }
